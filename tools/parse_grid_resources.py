035dec9986d8ab19434381281fa926352c5f0e74
Tom Lippincott <tom.lippincott@gmail.com>
2017-04-22 10:37:05 -0400

    Fixed resource parsers.

--- tools/parse_grid_resources.py
+++ tools/parse_grid_resources.py
@@ -5,33 +5,33 @@ import subprocess
 
 
 pattern = re.compile(r""".*
-ru_wallclock\s+(?P<wallclock>\S+)\s+
-ru_utime\s+(?P<usertime>\S+)\s+
-ru_stime\s+(?P<systemtime>\S+)\s+
-ru_maxrss\s+(?P<maxresidentset>\S+)\s+
-ru_ixrss\s+(\S+)\s+
-ru_ismrss\s+(\S+)\s+
-ru_idrss\s+(\S+)\s+
-ru_isrss\s+(\S+)\s+
-ru_minflt\s+(\S+)\s+
-ru_majflt\s+(\S+)\s+
-ru_nswap\s+(\S+)\s+
-ru_inblock\s+(\S+)\s+
-ru_oublock\s+(\S+)\s+
-ru_msgsnd\s+(\S+)\s+
-ru_msgrcv\s+(\S+)\s+
-ru_nsignals\s+(\S+)\s+
-ru_nvcsw\s+(\S+)\s+
-ru_nivcsw\s+(\S+)\s+
-wallclock\s+(\S+)\s+
-cpu\s+(\S+)\s+
-mem\s+(\S+)\s+
-io\s+(\S+)\s+
-iow\s+(\S+)\s+
-ioops\s+(\S+)\s+
-maxvmem\s+(\S+)\s+
-maxrss\s+(\S+)\s+
-maxpss\s+(\S+)\s+
+ru_wallclock\s+(?P<wallclock>[^\n]+)\s+
+ru_utime\s+(?P<usertime>[^\n]+)\s+
+ru_stime\s+(?P<systemtime>[^\n]+)\s+
+ru_maxrss\s+(?P<maxresidentset>[^\n]+)\s+
+ru_ixrss\s+([^\n]+)\s+
+ru_ismrss\s+([^\n]+)\s+
+ru_idrss\s+([^\n]+)\s+
+ru_isrss\s+([^\n]+)\s+
+ru_minflt\s+([^\n]+)\s+
+ru_majflt\s+([^\n]+)\s+
+ru_nswap\s+([^\n]+)\s+
+ru_inblock\s+([^\n]+)\s+
+ru_oublock\s+([^\n]+)\s+
+ru_msgsnd\s+([^\n]+)\s+
+ru_msgrcv\s+([^\n]+)\s+
+ru_nsignals\s+([^\n]+)\s+
+ru_nvcsw\s+([^\n]+)\s+
+ru_nivcsw\s+([^\n]+)\s+
+wallclock\s+([^\n]+)\s+
+cpu\s+([^\n]+)\s+
+mem\s+([^\n]+)\s+
+io\s+([^\n]+)\s+
+iow\s+([^\n]+)\s+
+ioops\s+([^\n]+)\s+
+maxvmem\s+([^\n]+)\s+
+maxrss\s+([^\n]+)\s+
+maxpss\s+([^\n]+)\s+
 .*
 """, re.DOTALL | re.VERBOSE)
 
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     p = subprocess.Popen(["qacct", "-j", "%d" % (options.id)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     
-    resources = pattern.match(err).groupdict()
+    resources = {k : float(v) for k, v in pattern.match(err + out).groupdict().iteritems()}
     
     with gzip.open(options.output, "w") as ofd:
         pickle.dump(resources, ofd)
--- tools/parse_local_resources.py
+++ tools/parse_local_resources.py
@@ -7,7 +7,7 @@ pattern = re.compile(r"""
 .*User\ time\ \(seconds\):\ (?P<usertime>\S+)\s+
 System\ time\ \(seconds\):\ (?P<systemtime>\S+)\s+
 Percent\ of\ CPU\ this\ job\ got:\ (?P<cpupercent>\S+)%\s+
-Elapsed\ \(wall\ clock\)\ time\ \(h:mm:ss\ or\ m:ss\):\ (?P<wallclock>\S+)\s+
+Elapsed\ \(wall\ clock\)\ time\ \(h:mm:ss\ or\ m:ss\):\ (\S+)\s+
 Average\ shared\ text\ size\ \(kbytes\):\ (\S+)\s+
 Average\ unshared\ data\ size\ \(kbytes\):\ (\S+)\s+
 Average\ stack\ size\ \(kbytes\):\ (?P<avgstack>\S+)\s+
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     options = parser.parse_args()
     
     with gzip.open(options.input) as ifd:
-        resources = pattern.match(ifd.read()).groupdict()
+        resources = {k : float(v) for k, v in pattern.match(ifd.read()).groupdict().iteritems()}
 
     with gzip.open(options.output, "w") as ofd:
         pickle.dump(resources, ofd)
