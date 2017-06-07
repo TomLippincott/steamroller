9a89e1bade0b56f79909a750fb353e763156e409
Tom Lippincott <tom.lippincott@gmail.com>
2017-06-06 20:47:58 -0400

    Added text-to-communication converter.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="SteamRoller",
-      version="1.0.3",
+      version="1.0.4",
       description="An experimental framework for text classification experiments",
       author="Tom Lippincott",
       author_email="tom@cs.jhu.edu",
@@ -18,7 +18,7 @@ setup(name="SteamRoller",
                 "steamroller.plots",
                 "steamroller.scons"],
       package_dir={"steamroller" : "src"},
-      scripts=["scripts/steamroller"],
+      scripts=["scripts/steamroller", "scripts/text_to_concrete.py"],
       install_requires=["concrete", "scikit-learn==0.18.1", "scons", "plotnine", "seaborn", "tensorflow", "valid", "flask"],
       package_data={"" : ["data/SConstruct", "data/steamroller_config.py.template"]},
      )
