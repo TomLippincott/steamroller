079793331ac493fbb46f7fbf35c3794e18646c34
Thomas Lippincott <tom.lippincott@gmail.com>
2017-08-01 12:09:50 -0400

    Update ppm.py

--- src/models/ppm.py
+++ src/models/ppm.py
@@ -27,17 +27,12 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     
     if options.train and options.output and options.input:
-        compressors = {}
         counts = {}
+        model = Classifier(options.max_ngram)
         for cid, label, text in read_data(options.input, options.train):
             counts[label] = counts.get(label, 0) + 1
-            compressors[label] = compressors.get(label, Compressor(options.max_ngram))
-            compressors[label].add(text)
-
-        model = Classifier(options.max_ngram)
-        for l, c in compressors.iteritems():
-            model.add(c, l)
-
+            model.train(label, text)
+        
         with gzip.open(options.output, "w") as ofd:
             pickle.dump((model, counts), ofd)
 
