69f36a56a5a46b522c11ce67dec85ce5e0b25a8f
Hope McGovern <hem52@cam.ac.uk>
2024-10-21 13:41:25 +0100

    fixing imports in univa_engine.py

--- src/steamroller/engines/univa_engine.py
+++ src/steamroller/engines/univa_engine.py
@@ -1,3 +1,7 @@
+import os
+import subprocess
+import logging
+import shlex
 from .grid_engine import GridEngine
 
 #def univa(commands, name, std, dep_ids=[], grid_resources=[], working_dir=None, queue="all.q"):
@@ -10,7 +14,7 @@ def univa(commands, name, std, dep_ids=[], working_dir=None, gpu_count=0, time="
         except:
             pass
     deps = "" if len(dep_ids) == 0 else "-hold_jid {}".format(",".join([str(x) for x in dep_ids]))
-    res = "" if len(grid_resources) == 0 else "-l {}".format(",".join([str(x) for x in grid_resources]))
+    #res = "" if len(grid_resources) == 0 else "-l {}".format(",".join([str(x) for x in grid_resources]))
     wd = "-wd {}".format(working_dir) if working_dir else "-cwd"
     qcommand = "qsub -terse -shell n -V -N {} -q {} -b n {} {} -j y -o {} -l h_rt={},mem_free={}".format(name, queue, wd, deps, std, time, memory)
     logging.info("\n".join(commands))
